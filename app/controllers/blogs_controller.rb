d22feef8b48f2c97071366a58b00c912c30a1306
AikaOnimura <aika.onimura@gmail.com>
2020-09-22 12:48:51 +0000

    [Start/Add]3-4 ブログの自動更新/controllerファイルに全データ取得のアクション追加、HTMLにループ機能追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,10 @@
 class BlogsController < ApplicationController
-  def index
-  end
 
   def show
+  end
+  
+  def index #top画面に対応するのはindexアクション
+      @blogs = Blog.all #全ての情報を取得
   end
 
   def new
