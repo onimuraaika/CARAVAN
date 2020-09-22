0a4eb115b54067d62964324dde59b46ce50b336d
AikaOnimura <aika.onimura@gmail.com>
2020-09-22 06:22:45 +0000

    [Start/Add]3-3 投稿機能の追加/createアクションの追加、新規投稿用blogインスタンスの作成、HTMLに投稿フォームの追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,6 +7,10 @@ class BlogsController < ApplicationController
 
   def new
   end
+  
+  def create
+    @blog = Blog.new #form_with(新規投稿用フォーム)を使うにはインスタンス変数が必要。新規データ登録用のblogインスタンスを、Blogモデルから作成。『Blog.new」で空のモデルが生成される。
+  end
 
   def edit
   end
