853dfc552bc1df0823f3155ebf34f303df17c305
AikaOnimura <aika.onimura@gmail.com>
2020-09-23 06:05:28 +0000

    [Add]3-5 コメントの追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,12 +8,12 @@ class BlogsController < ApplicationController
       @blogs = Blog.all #全ての情報を取得して@blogsに入れる
   end
 
-  def new
+  def new #新規投稿用フォーム表示アクション
       @blog = Blog.new #「Blog.new」で空のモデルが生成される。
   end
   
-  def create
-      blog = Blog.new(blog_params) #モデル名がblog➡️アクション名はblog_params。
+  def create #送信された情報を保存するアクション
+      blog = Blog.new(blog_params) #viewに渡す必要がないから普通の変数
       blog.save
       redirect_to blog_path(blog.id) #詳細表示画面に飛ぶ。パス名は$rails routesで確認できる
   end
